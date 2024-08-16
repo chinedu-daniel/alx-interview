78051865a2cc7caf0d0f663a1d01afe3fd1aab7b
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-16 20:56:45 +0100

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,23 +3,21 @@
 0-pascal_triangle
 """
 
+
 def pascal_triangle(n):
     """
     Returns a list of integers
     """
-
     if n <= 0:
         return []
 
     triangle = []
-        for i in range(n):
-            row = [1]
 
-            for j in range(1 - i):
-                row.append(triangle[i - 1][j-1] + triangle[i - 1][j])
+    for i in range(n):
+        row = [1]
+        for j in range(1 - i):
+            row.append(triangle[i - 1][j-1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
 
-            for i > 0:
-                row.append(1)
-            triangle.append(row)
-
-        return triangle
+    return triangle
